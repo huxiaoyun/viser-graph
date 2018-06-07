9c53b8b8a5d449a24ae053f366ccde12d071692f
ludy.hxy <huxiaoyunhust@gmail.com>
2018-06-07 15:26:04 +0800

    fix: remove editor and remove es lib umd folder

--- packages/viser-graph-react/src/index.ts
+++ packages/viser-graph-react/src/index.ts
@@ -1,25 +1,9 @@
 import { default as Graph } from './components/Graph';
 import { Zoom } from './components/SubComponent';
 
-import Editor from './editor/editor';
-import Navigator from './editor/navigator';
-import DetailPannel from './editor/detailpannel';
-import ItemPannel from './editor/itempannel';
-import Toolbar from './editor/toolbar';
-
-import { registerNode, command } from './editor/editor';
-
 import 'antd/dist/antd.css';
 
 export {
   Graph,
   Zoom,
-  Editor,
-  Navigator,
-  DetailPannel,
-  ItemPannel,
-  Toolbar,
-
-  registerNode,
-  command,
 };
